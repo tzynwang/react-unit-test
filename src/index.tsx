a72c5f1f1e71d443b55ea460a764beb401392ea9
tzyn <tzyn.wang@gmail.com>
2022-03-06 15:38:08 +0800

    [feat] Update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@mui/material/styles'
 
 import App from '@Components/App'
 import ErrorBoundary from '@Components/Common/ErrorBoundary'
+import theme from '@Base/theme'
 
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
